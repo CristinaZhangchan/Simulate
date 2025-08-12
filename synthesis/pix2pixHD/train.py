9e6a528a19f166fdf166a5dc0f134934f7b2e3ae
Cristina Zhang <73481741+CristinaZhangchan@users.noreply.github.com>
2025-08-12 23:13:55 +0200

    Update train.py

--- synthesis/pix2pixHD/train.py
+++ synthesis/pix2pixHD/train.py
@@ -58,7 +58,11 @@ if opt.fp16:
     model, [optimizer_G, optimizer_D] = amp.initialize(model, [model.optimizer_G, model.optimizer_D], opt_level='O1')             
     model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
 else:
-    optimizer_G, optimizer_D = (model.module.optimizer_G, model.module.optimizer_D) if torch.cuda.is_available() else (model.optimizer_G, model.optimizer_D)
+   if hasattr(model, 'module'):
+    optimizer_G, optimizer_D = model.module.optimizer_G, model.module.optimizer_D
+else:
+    optimizer_G, optimizer_D = model.optimizer_G, model.optimizer_D
+
 
 total_steps = (start_epoch-1) * dataset_size + epoch_iter
 
